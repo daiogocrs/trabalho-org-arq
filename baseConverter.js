c33a8e5473747a4e3c98ed7a05d5a9e386ec46b3
joaov <joaovitorwagnerp@gmail.com>
2025-04-12 16:25:02 -0300

    refactor: modularização e simplificação do código

--- baseConverter.js
+++ baseConverter.js
@@ -1,108 +1,58 @@
 function converter(input = 0, base_origem, base_final) {
-  let resultado = 0,
-    potencia = 0,
-    passos = "",
-    potencia2 = input.length - 1;
+  let resultado = 0;
+  let passos = "";
 
   switch (base_origem) {
     case 2:
-      for (let i = input.length - 1; i >= 0; i--) {
-        resultado += input[i] * Math.pow(2, potencia);
-        potencia++;
-      }
-
-      for (let j = 0; j < input.length; j++) {
-        passos += `${input[j]} x 2^${potencia2}`;
-        if (potencia2 > 0) passos += " + ";
-        potencia2--;
-      }
-
+      resultado = toDecimal(input, 2);
+      passos = mostrarPassos(input, 2);
       console.log(`\nBIN(${input}) => DEC(${resultado}) \n` + passos + "\n");
       break;
 
     case 8:
-      for (let i = input.length - 1; i >= 0; i--) {
-        resultado += input[i] * Math.pow(8, potencia);
-        potencia++;
-      }
-
-      for (let j = 0; j < input.length; j++) {
-        passos += `${input[j]} x 8^${potencia2}`;
-        if (potencia2 > 0) passos += " + ";
-        potencia2--;
-      }
-
+      resultado = toDecimal(input, 8);
+      passos = mostrarPassos(input, 8);
       console.log(`\nOCT(${input}) => DEC(${resultado}) \n` + passos + "\n");
       break;
 
     case 10:
-      console.log("Base origem: Decimal");
-      resultado = input;
-      console.log(`\nDEC(${input}) => DEC(${resultado}) \n` + passos + "\n");
+      resultado = parseInt(input, 10);
+      console.log(`\nDEC(${input}) => DEC(${resultado})\n`);
       break;
 
     case 16:
-      console.log("Base origem: Hexa-Decimal");
-
-      for (let i = input.length - 1; i >= 0; i--) {
-        switch (input[i]) {
-          case "A":
-            resultado += 10 * Math.pow(16, potencia);
-            break;
-          case "B":
-            resultado += 11 * Math.pow(16, potencia);
-            break;
-          case "C":
-            resultado += 12 * Math.pow(16, potencia);
-            break;
-          case "D":
-            resultado += 13 * Math.pow(16, potencia);
-            break;
-          case "E":
-            resultado += 14 * Math.pow(16, potencia);
-            break;
-          case "F":
-            resultado += 15 * Math.pow(16, potencia);
-            break;
-          default:
-            resultado += input[i] * Math.pow(16, potencia);
-            break;
-        }
-        potencia++;
-      }
-
-      for (let j = 0; j < input.length; j++) {
-        let tmp;
-        switch (input[j]) {
-          case "A":
-            tmp = "10";
-            break;
-          case "B":
-            tmp = "11";
-            break;
-          case "C":
-            tmp = "12";
-            break;
-          case "D":
-            tmp = "13";
-            break;
-          case "E":
-            tmp = "14";
-            break;
-          case "F":
-            tmp = "15";
-            break;
-          default:
-            tmp = input[j];
-            break;
-        }
-        passos += `${tmp} x 16^${potencia2}`;
-        if (potencia2 > 0) passos += " + ";
-        potencia2--;
-      }
+      resultado = toDecimal(input, 16);
+      passos = mostrarPassos(input, 16);
       console.log(`\nHEX(${input}) => DEC(${resultado}) \n` + passos + "\n");
       break;
   }
 }
 
-converter("66", 8, 10);
+function toDecimal(input, base) {
+  let resultado = 0;
+  let potencia = 0;
+
+  for (let i = input.length - 1; i >= 0; i--) {
+    let valor = parseInt(input[i], base);
+    resultado += valor * Math.pow(base, potencia);
+    potencia++;
+  }
+  console.log(resultado);
+  return resultado;
+}
+
+function mostrarPassos(input, base) {
+  let passos = "";
+  let potencia = input.length - 1;
+
+  for (let i = 0; i < input.length; i++) {
+    let valor = parseInt(input[i], base);
+    passos += `${valor} x ${base}^${potencia}`;
+    if (potencia > 0) passos += " + ";
+    potencia--;
+  }
+
+  return passos;
+}
+
+converter("ABCDEF", 16, 1226);
